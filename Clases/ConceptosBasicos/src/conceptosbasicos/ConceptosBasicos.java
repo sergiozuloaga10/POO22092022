db53ec18908910c4947cd2884db6b8906c441d46
sergiozuloaga10 <sergio-sst@hotmail.com>
2022-03-02 18:37:22 -0600

    Sesion martes 1deMarzo, automovil

--- Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.fes.Automovil;
+import ico.fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author Sergio
@@ -16,6 +20,23 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println(s1);//imprimira la direccion de memoria 
+        System.out.println(s1.toString());//imprime lo mismo
+        
+        Automovil miBocho = new Automovil();//constructor por defecto
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.BLUE);
+        System.out.println(miBocho);
+        
+        //con constructor sobre cargado crtl + tecla espacio
+        Automovil miMustan = new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(miMustan);
+        
+        Automovil miAkura = new Automovil("Akura", "NSX", 2017, Color.GRAY);
+        System.out.println(miAkura);
     }
     
 }
---
+++ Clases/ConceptosBasicos/src/ico/fes/Automovil.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Automovil {
+     //creamos atributos de la clase
+   private String marca; //los atributos van en notacion lower camel case
+   private String subMarca;//lower camel case todas en minusculas y apartir 
+                   //de la segunda palabra, inicia con mayusculas
+   private int modelo;
+   private Color color;
+
+    public Automovil() {
+    }
+
+    public Automovil(String marca, String subMarca, int modelo, Color color) {
+        this.marca = marca;
+        this.subMarca = subMarca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSubMarca() {
+        return subMarca;
+    }
+
+    public void setSubMarca(String subMarca) {
+        this.subMarca = subMarca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override //-herencia -> esta sobre escribiendo el metodo en una clase
+    public String toString() {
+        return "Automovil{" + "marca=" + marca + ", subMarca=" + subMarca + ", modelo=" + modelo + ", color=" + color + '}';
+    }
+   
+   
+}
