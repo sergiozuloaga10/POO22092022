03472cc235475c2123ebca1f4b1ca4967c37a541
sergiozuloaga10 <sergio-sst@hotmail.com>
2022-05-12 13:23:22 -0500

    Subimos clase 12/05/2022/

--- Clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
+++ Clases/JavaBasico2209Parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -6,6 +6,7 @@ package javabasico2209parte2;
 
 import ico.fes.awt.Ventana;
 import ico.fes.awt.VentanaVersion2;
+import ico.fes.swing.VentanaSwing;
 
 /**
  *
@@ -15,8 +16,10 @@ public class JavaGraficoMain {
     public static void main(String[] args) {
         
        // Ventana ventana = new Ventana("Hola AWT ICO FES ARAGON");
-        VentanaVersion2 ventana = new VentanaVersion2();
+       // VentanaVersion2 ventana = new VentanaVersion2();
         
+        VentanaSwing vs = new VentanaSwing();
+        System.out.println(System.getProperty("user.dir"));
         
     }
 }
